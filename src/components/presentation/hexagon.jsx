df246e6d5c65cd7fd5fd0a56b93f96f2e3764f02
juan heza <juanehza@hotmail.com>
2021-10-20 23:54:52 -0500

    reparacion de intro

--- src/components/presentation/hexagon.jsx
+++ src/components/presentation/hexagon.jsx
@@ -4,23 +4,28 @@ import React from 'react'
 class Hexagon extends React.Component {
   render(){
     return(
-      <div className="coin">
+      <div className="coin" id={this.props.id}>
         <svg className="face" xmlns="http://www.w3.org/2000/svg" version="1.1" height="100%" width='100%' viewBox="0 0 300 300">
           <polygon className="border" transform="rotate(270 0 0)" transform-origin="center" points="300,150 225,280 75,280 0,150 75,20 225,20"></polygon>
           <polygon className="hex" transform="scale(0.85) rotate(270 0 0)" transform-origin="center" points="300,150 225,280 75,280 0,150 75,20 225,20"></polygon>
-          <image href="/icons/59.png" x="20%" y="20%" height="60%" width="60%"></image>
+          <image href={this.props.front} x="20%" y="20%" height="60%" width="60%"></image>
         </svg>
 
         <svg className="face backface" xmlns="http://www.w3.org/2000/svg" version="1.1" height="100%" width='100%' viewBox="0 0 300 300">
           <polygon className="border" transform="rotate(270 0 0)" transform-origin="center" points="300,150 225,280 75,280 0,150 75,20 225,20"></polygon>
           <polygon className="hex" transform="scale(0.85) rotate(270 0 0)" transform-origin="center" points="300,150 225,280 75,280 0,150 75,20 225,20"></polygon>
-          <image href="" x="20%" y="20%" height="60%" width="60%"></image>
+          <image href={this.props.back} x="20%" y="20%" height="60%" width="60%"></image>
         </svg>
       </div>
     )
   }
 }
-
+Hexagon.defaultProps = {
+  front: "",
+  back: "",
+  id: "",
+  // colors: ""4
+}
 class HexagonImage extends React.Component{
   render(){
     return(
@@ -37,36 +42,29 @@ class Honeycomb extends React.Component {
             <div className="honeycomb">
                 <div className="row border" >
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h1.bi} alt="logo" id="honey-comb-1"/>
+                    <Hexagon front={this.props.data.h1.front} id="honey-comb-1" />
                   </div>
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h2.bi} alt="logo" id="honey-comb-2"/>
+                    <Hexagon front={this.props.data.h2.front} id="honey-comb-2" />
                   </div>
                 </div>
                 <div className="row middle">
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h3.bi} alt="logo" id="honey-comb-3"/>
+                    <Hexagon front={this.props.data.h3.front} id="honey-comb-3" />
                   </div>
                   <div id="center-hexagon" className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h4.bi} alt="logo" id="honey-comb-4"/>  
+                    <Hexagon back={this.props.data.h4.back} id="honey-comb-4" />
                   </div>
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h5.bi} alt="logo" id="honey-comb-5"/>  
+                    <Hexagon front={this.props.data.h5.front} id="honey-comb-5" />
                   </div>
                 </div>
                 <div className="row border" >
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h6.bi} alt="logo" id="honey-comb-6"/>  
+                    <Hexagon front={this.props.data.h6.front} id="honey-comb-6" />
                   </div>
                   <div className="hexagon-container" >
-                    <Hexagon />
-                    <img src={this.props.data.h7.bi} alt="logo" id="honey-comb-7"/>  
+                    <Hexagon front={this.props.data.h7.front} id="honey-comb-7" />
                   </div>
                 </div>
                 
--- src/components/presentation/loading.jsx
+++ src/components/presentation/loading.jsx
@@ -1,7 +1,7 @@
 import './loading.css';
 import Honeycomb, {Hexagon} from "./hexagon"
 import React from 'react'
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 
 class Loading extends React.Component {
   constructor(props){
@@ -22,13 +22,13 @@ class Loading extends React.Component {
     return (
       <div class="loading">
         <Honeycomb data={{
-              h1:{bi: "./icons/aarch64-original.svg"},
-              h2:{bi: "./icons/aftereffects-original.svg"},
-              h3:{bi: "./icons/amazonwebservices-original.svg"},
-              h4:{bi: "./icons/android-original.svg"},
-              h5:{bi: "./icons/angularjs-original.svg"},
-              h6:{bi: "./icons/apache-original.svg"},
-              h7:{bi: "./icons/appcelerator-original.svg"},
+              h1:{front: "./icons/aarch64-original.svg"},
+              h2:{front: "./icons/aftereffects-original.svg"},
+              h3:{front: "./icons/amazonwebservices-original.svg"},
+              h4:{back: "./icons/android-original.svg"},
+              h5:{front: "./icons/angularjs-original.svg"},
+              h6:{front: "./icons/apache-original.svg"},
+              h7:{front: "./icons/appcelerator-original.svg"},
           }}/>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <div className="lema">
@@ -36,7 +36,7 @@ class Loading extends React.Component {
             <div id="row-2" className="lema-row"> <span class="hexal"><Hexagon /></span > <span className="text">Design</span>  <span class="hexal"><Hexagon /></span> </div>
             <div id="row-3" className="lema-row"> <span class="hexal"><Hexagon /></span > <span className="text">Create</span>  <span class="hexal"><Hexagon /></span> </div>
           </div>
-          {this.state.redirect && <Redirect to="/Home" /> }
+          {/* {this.state.redirect && <Redirect to="/Home" /> } */}
       </div>
     );
   }
