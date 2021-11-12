5201ca764aa7f49bd166f8a6751d9f44b6e549c2
juan heza <juanehza@hotmail.com>
2021-11-12 00:26:29 -0600

    responsive home

--- src/components/about/intro.jsx
+++ src/components/about/intro.jsx
@@ -5,7 +5,7 @@ export default class Intro extends React.Component{
     render(){
         return(
             <section id="intro" className="flex row wrap jc-evenly" >
-                <div className=" identity ta-center flex column js-center ai-center">
+                <div className=" identity ta-center flex column wrap js-center ai-center">
                     <Hexagon />
                     <h2 className="name ">Juan Enrique Hernandez Zavala</h2>
                     <h4 className="grade">Ingeniero en Tecnologia de Software</h4>
